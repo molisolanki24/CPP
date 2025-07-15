b73e6ccc471ddde2bc867e7bb5dd35507495dfac
molisolanki24 <molisolanki292405@gmail.com>
2025-07-15 10:03:15 +0530

    Update Calculator.h

--- Calculator With Header File/Calculator.h
+++ Calculator With Header File/Calculator.h
@@ -1,3 +1,42 @@
 #include <iostream>
 
-void calculator();
+ using namespace std;
+
+ inline void show(){	
+ 	 
+ 	 double A,B;
+ 	 char Operator;
+ 	 
+ 	 cout<<"Enter First Value Of A: ";
+ 	 cin>>A;
+ 	 cout<<"Enter Second Value Of B: ";
+ 	 cin>>B;
+ 	 cout<<"Enter Operator(+,-,*,/): ";
+ 	 cin>>Operator;
+
+ 
+ switch(Operator) {
+ 	case '+':
+ 	 cout<<"Addition: "<<A+B;
+     break;
+ 		
+ 	case '-':
+	 cout<<"Substraction: "<<A-B;
+	 break;
+	 
+	case '*':
+	 cout<<"Multiplication: "<<A*B;
+	 break;
+	 
+	case '/':
+	 if(B!=0){
+	 	cout<<"Division: "<<A/B;
+	 }else{
+	 	cout<<"Invalid Division!!";
+	 break;
+}
+	default:
+	 cout<<"Error!!.. invalid Operator";  	
+  
+ }
+ }
